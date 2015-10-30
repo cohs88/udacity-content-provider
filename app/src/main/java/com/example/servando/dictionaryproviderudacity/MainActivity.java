7d37560a5dc37881098bbcec9a251f127d356c98
servando coria <cohs88@gmail.com>
2015-10-29 21:43:30 -0700

    usando un listview y adapter

--- app/src/main/java/com/example/servando/dictionaryproviderudacity/MainActivity.java
+++ app/src/main/java/com/example/servando/dictionaryproviderudacity/MainActivity.java
@@ -7,17 +7,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String[] COLUMNS_TO_BE_BOUND = new String[]{
+            UserDictionary.Words.WORD,
+            UserDictionary.Words.FREQUENCY
+    };
+
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[]{
+            android.R.id.text1,
+            android.R.id.text2
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // get the listview
+        ListView dictionaryListView = (ListView)findViewById(R.id.dictionary_list_view);
+
         // get the textview
-        TextView dictionaryTextView = (TextView)findViewById(R.id.dictionary_text_view);
+        //TextView dictionaryTextView = (TextView)findViewById(R.id.dictionary_text_view);
 
         // obtener el content resolver que enviara el mensaje al content provide
         ContentResolver contentResolver = getContentResolver();
@@ -25,39 +39,11 @@ public class MainActivity extends AppCompatActivity {
         // cursor con todos los records in la tabla Words
         Cursor cursor = contentResolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
+        // creamos el adapter con un layout predefinido de android (two_line_list_item)
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, cursor, COLUMNS_TO_BE_BOUND, LAYOUT_ITEMS_TO_FILL, 0);
 
-        try {
-            dictionaryTextView.setText("The UserDictionary contains " + Integer.toString(cursor.getCount()) + " words\n");
-            dictionaryTextView.append("Columns " + UserDictionary.Words._ID + " - " + UserDictionary.Words.FREQUENCY + " - " + UserDictionary.Words.WORD + "\n");
-
-            //cursor.moveToNext();
-
-            int idColumnIndex = cursor.getColumnIndex(UserDictionary.Words._ID);
-            int frequencyColumnIndex = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
-            int wordColumnIndex = cursor.getColumnIndex(UserDictionary.Words.WORD);
-
-            int idColumn = 0;
-            int frequencyColumn = 0;
-            String wordColumn = "";
-
-            while(cursor.moveToNext())
-            {
-                idColumn =  cursor.getInt(idColumnIndex);
-
-                frequencyColumn = cursor.getInt(frequencyColumnIndex);
-
-                wordColumn = cursor.getString(wordColumnIndex);
-
-                dictionaryTextView.append(Integer.toString(idColumn) + " " + Integer.toString(frequencyColumn) + " " + wordColumn + "\n");
-            }
-        }
-        catch (Exception ex)
-        {
-
-        }
-        finally {
-            cursor.close();
-        }
+        // ponemos el adapter
+        dictionaryListView.setAdapter(simpleCursorAdapter);
     }
 
     @Override
